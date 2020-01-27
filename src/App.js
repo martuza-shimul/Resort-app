002c98c305d3c0177a2ef5417920120c32bb845d
Martuza <martuza.shimul@gmail.com>
2020-01-27 19:41:06 +0600

    navbar component added and designed

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ import Error from "./pages/Error";
 
 import { Route, Switch } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
+
 function App() {
 	return (
 		<>
+			<Navbar />
 			<Switch>
 				<Route exact path="/" component={Home} />
 				<Route exact path="/rooms" component={Rooms} />
