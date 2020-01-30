5896067e49681f39cc3dfd019a1383399b6b4a75
Martuza <martuza.shimul@gmail.com>
2020-01-31 03:57:33 +0600

    room component configured

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import defaultImg from "../images/room-1.jpeg";
 
-const Room = () => {
-	return <div>Hello from Room Component</div>;
+const Room = ({ room }) => {
+	// console.log(room);
+	let { name, slug, price, images } = room;
+	return (
+		<>
+			<article className="room">
+				<div className="img-container">
+					<img src={images[0] || defaultImg} alt="single room" />
+					<div className="price-top">
+						<h6>${price}</h6>
+						<p>per night</p>
+					</div>
+					<Link to={`/rooms/${slug}`} className="btn-primary room-link">
+						Features
+					</Link>
+				</div>
+				<p className="room-info">{name}</p>
+			</article>
+		</>
+	);
 };
 
 export default Room;
