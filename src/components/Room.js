eaad7f9143d15c7baa097bef294f7375ed89446a
Martuza <martuza.shimul@gmail.com>
2020-01-31 04:02:21 +0600

    propTypes added to the room component

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import defaultImg from "../images/room-1.jpeg";
+import PropTypes from "prop-types";
 
 const Room = ({ room }) => {
 	// console.log(room);
@@ -22,6 +23,15 @@ const Room = ({ room }) => {
 			</article>
 		</>
 	);
+};
+
+Room.prototype = {
+	room: PropTypes.shape({
+		name: PropTypes.string.isRequired,
+		slug: PropTypes.string.isRequired,
+		images: PropTypes.arrayOf(PropTypes.string).isRequired,
+		price: PropTypes.number.isRequired
+	})
 };
 
 export default Room;
