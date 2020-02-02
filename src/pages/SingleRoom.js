175af99b8b51ac6227533996d09009ecea8a5a73
Martuza <martuza.shimul@gmail.com>
2020-02-02 23:58:52 +0600

    context added to single page

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
+import defaultBcg from "../images/room-1.jpeg";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import { RoomContext } from "../context";
 
 export default class SingleRoom extends Component {
+	constructor(props) {
+		super(props);
+		// console.log(this.props);
+
+		this.state = {
+			slug: this.props.match.params.slug,
+			defaultBcg
+		};
+	}
+	static contextType = RoomContext;
 	render() {
+		const { getRoom } = this.context;
+		const room = getRoom(this.state.slug);
+		console.log(room);
 		return <div>Single Room</div>;
 	}
 }
